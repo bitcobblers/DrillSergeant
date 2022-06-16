b986506b7e00d48148f915e609510f6d96ada83e
Michael Vastarelli <michael.vastarelli@gmail.com>
2022-06-15 23:42:33 -0400

    Added skeletal test adapter.

---
+++ src/JustBehave.TestAdapter/Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace JustBehave.TestAdapter;
+
+public static class Constants
+{
+    public const string ExecutorUri = "executor://JustBehave";
+}
---
+++ src/JustBehave.TestAdapter/JustTestExecutor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using System.Collections.Generic;
+
+namespace JustBehave.TestAdapter
+{
+    [ExtensionUri(Constants.ExecutorUri)]
+    public class JustTestExecutor : ITestExecutor
+    {
+        public void Cancel()
+        {
+        }
+
+        public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            frameworkHandle.SendMessage(TestMessageLevel.Warning, "JustBehave test executor not implemented.");
+        }
+
+        public void RunTests(IEnumerable<string> sources, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            frameworkHandle.SendMessage(TestMessageLevel.Warning, "JustBehave test executor not implemented.");
+        }
+    }
+}
