b90adefaaa02c268dc962e966bd2baa00cfbc2e1
Michael Vastarelli <michael.vastarelli@gmail.com>
2023-06-10 22:16:56 -0400

    Added feature tests for base step.

---
+++ test/DrillSergeant.Tests/Features/BaseStepFeature.cs
@@ -0,0 +1,20 @@
+﻿using DrillSergeant.GWT;
+using Xunit;
+
+namespace DrillSergeant.Tests.Features;
+
+public class BaseStepFeature
+{
+    [Behavior, InlineData(0)]
+    public Behavior ModifyingInputFails(int _)
+    {
+        var input = new
+        {
+            Value = "expected"
+        };
+
+        return new Behavior(input)
+            .When("Update input", (c, i) => i.Value = "error")
+            .Then("Input should be unchanged", (c, i) => Assert.Equal("expected", i.Value));
+    }
+}
