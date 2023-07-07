922b4d62eb570d368a3dd4fd7e1f49e7164ce28e
Michael Vastarelli <michael.vastarelli@gmail.com>
2023-07-07 00:22:10 -0400

    Created projects.

---
+++ src/DrillSergeant.MSTest/BehaviorAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// ReSharper disable once CheckNamespace
+namespace DrillSergeant;
+
+/// <summary>
+/// Defines an attribute used to notify the test runner that the method is a behavior test.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class BehaviorAttribute : TestMethodAttribute 
+{
+    public string? Feature { get; set; }
+}
