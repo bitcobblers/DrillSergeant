9725ae788d13d01cef3ba32cbae695ddfe8a2117
Michael Vastarelli <michael.vastarelli@gmail.com>
2023-07-06 21:51:38 -0400

    Feature property now turns into a category automatically.

--- src/DrillSergeant.NUnit3/BehaviorAttribute.cs
+++ src/DrillSergeant.NUnit3/BehaviorAttribute.cs
@@ -1,7 +1,8 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using DrillSergeant.NUnit3;
+﻿using DrillSergeant.NUnit3;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Internal.Builders;
 using NUnit.Framework.Internal.Commands;
 
 // ReSharper disable once CheckNamespace
@@ -10,9 +11,28 @@ namespace DrillSergeant;
 /// <summary>
 /// Defines an attribute used to notify the test runner that the method is a behavior test.
 /// </summary>
-[ExcludeFromCodeCoverage, AttributeUsage(AttributeTargets.Method)]
-public sealed class BehaviorAttribute : TestAttribute, IWrapTestMethod
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class BehaviorAttribute : NUnitAttribute, IWrapTestMethod, ISimpleTestBuilder, IApplyToTest, IImplyFixture
 {
+    private readonly NUnitTestCaseBuilder _builder = new ();
+
     public string? Feature { get; set; }
-    public TestCommand Wrap(TestCommand command) => new BehaviorCommand(command.Test);
+
+    public TestMethod BuildFrom(IMethodInfo method, Test? suite)
+    {
+        return _builder.BuildTestMethod(method, suite, null);
+    }
+
+    public void ApplyToTest(Test test)
+    {
+        if (string.IsNullOrWhiteSpace(Feature) == false)
+        {
+            test.Properties.Add(PropertyNames.Category, Feature);
+        }
+    }
+
+    public TestCommand Wrap(TestCommand command)
+    {
+        return new BehaviorCommand(command.Test);
+    }
 }
