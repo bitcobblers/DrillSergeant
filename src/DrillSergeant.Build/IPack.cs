75b2ee4cbbf6533bb72c3cb027368ed4903a497d
Michael Vastarelli <michael.vastarelli@gmail.com>
2023-09-29 00:07:01 -0400

    Fixed packing configuration.

--- src/DrillSergeant.Build/IPack.cs
+++ src/DrillSergeant.Build/IPack.cs
@@ -1,10 +1,10 @@
 using JetBrains.Annotations;
 using Nuke.Common;
 using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
-using Serilog;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 // ReSharper disable AllUnderscoreLocalParameterName
 // ReSharper disable VariableHidesOuterVariable
@@ -12,7 +12,7 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
 namespace DrillSergeant.Build;
 
 [PublicAPI]
-public interface IPack : ITest, IHaveArtifacts
+public interface IPack : ITest
 {
     AbsolutePath PackagesDirectory => ArtifactDirectory / "packages";
 
@@ -29,13 +29,18 @@ public interface IPack : ITest, IHaveArtifacts
                 .AddPair("Packages", PackagesDirectory.GlobFiles("*.nupkg").Count.ToString()));
         });
 
+    IEnumerable<Project> PackProjects =>
+        from p in Solution.GetAllProjects("*")
+        where p.GetProperty<string>("IsPackable") == "true"
+        select p;
+
     Configure<DotNetPackSettings> PackSettings => _ => _
         .SetProject(Solution)
+        .SetVersion(GitVersion.SemVer)
         .SetConfiguration(Configuration)
+        .SetOutputDirectory(PackagesDirectory)
         .SetNoBuild(SucceededTargets.Contains(Compile))
         .EnableNoLogo()
         .EnableNoRestore()
-        .EnableContinuousIntegrationBuild()
-        .SetVersion(GitVersion.SemVer)
-        .SetOutputDirectory(PackagesDirectory);
+        .EnableContinuousIntegrationBuild();
 }
