8bd7527b3e354d96a4cefe61f0f56f302ce30bc7
Michael Vastarelli <mike@bitcobblers.com>
2023-10-21 18:43:09 -0400

    Merge with main prior to cutting release. (#90)
    * Added additional path filters and misc. cleanups. (#84)
    
    * Added additional path filters and misc. cleanups.
    
    * Pull requests will always force CICD to run.
    
    * Code coverage runs as part of CI now.
    
    * Created signing key.
    
    * Updated package versions. (#85)
    
    * Feature/add signing key (#86)
    
    * Created signing key.
    
    * All assemblies (except _build) are now signed.
    
    * Fixed syntax error in conditional.
    
    * Disabled signing for build projects.
    
    * Add quality scanning to CodeQL (#87)
    
    * Added quality scanning to CodeQL.
    
    * Update .github/workflows/codeql.yml
    
    * Updated package versions. (#85)
    
    * Feature/add signing key (#86)
    
    * Created signing key.
    
    * All assemblies (except _build) are now signed.
    
    * Fixed syntax error in conditional.
    
    * Disabled signing for build projects.
    
    * Verb is now set when adding normal steps. (#88)
    
    * Fixed syntax issues. (#89)
    
    * Fixed syntax issues.
    
    * CodeQL will run for all main/dev commits.
    
    * Updated trigger filters.
    
    * Removed invalid args.
    
    * Add quality scanning to CodeQL (#87)
    
    * Added quality scanning to CodeQL.
    
    * Update .github/workflows/codeql.yml
    
    * Verb is now set when adding normal steps. (#88)
    
    * Fixed syntax issues. (#89)
    
    * Fixed syntax issues.
    
    * CodeQL will run for all main/dev commits.
    
    * Updated trigger filters.
    
    * Removed invalid args.

--- src/DrillSergeant/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("DrillSergeant.Tests")]
-[assembly: InternalsVisibleTo("DrillSergeant.Xunit2")]
-[assembly: InternalsVisibleTo("DrillSergeant.NUnit3")]
-[assembly: InternalsVisibleTo("DrillSergeant.MSTest")]
-[assembly: InternalsVisibleTo("DrillSergeant.Xunit2.Tests")]
-[assembly: InternalsVisibleTo("DrillSergeant.NUnit3.Tests")]
-[assembly: InternalsVisibleTo("DrillSergeant.MSTest.Tests")]
