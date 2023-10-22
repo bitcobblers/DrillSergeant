721aa35703431e0c649629155b1841c0ee9550f0
Michael Vastarelli <mike@bitcobblers.com>
2023-10-22 16:17:06 -0400

    Feature/overhaul build system (#91)
    * Re-wrote workflows without Nuke
    
    * Fixed multi-line issue.  Fixed codeql query set.
    
    * Fixed syntax error.
    
    * Fixed syntax issues.  Added cicd as a dependent step.  Added diag step to troubleshoot codeql trigger.
    
    * Fixed version formatting.
    
    * CodeQL is disabled by default for workflow_dispatch.
    
    * Removed dummy step.
    
    * Enabled fast-fail.
    
    * Configured outputs for workflow.
    
    * Dropped --no-logo params.
    
    * Wrapped output saving with debug code.
    
    * Fixed output variable mapping.
    
    * Added diag step for calculating versions.
    
    * Output caching now uses Add-Content instead of Write-Output.
    
    * Changed syntax of output saving.
    
    * Added NoBom encoding to output params.
    
    * Fixed output env variable.
    
    * Deleted debug statement.
    
    * Restrutured build commands.
    
    * Deleted diag step.
    
    * Changed line encoding style.
    
    * Switched line mode to folded.
    
    * Eliminated multi-line statements.
    
    * Added missing checkout step when creating tags.
    
    * Fixed syntax issue with create-release.  Switched push-packages to bash.
    
    * Removed old code.  Fixed tag name error in create-release.
    
    * Broke steps apart into reusable workflows.
    
    * Removed unneeded dependency.
    
    * Fixed variable reference.
    
    * Deleted old code.  Re-enabled multi-line scripts.
    
    * Added missing param definition.
    
    * Removed multi-line build statements.
    
    * Enabled nuget caching.
    
    * Disabled caching for nuget.
    
    * restored nuget push command in push-packages.
    
    * Removed Nuke from the solution.

--- build/Build.cs
+++
@@ -1,9 +0,0 @@
-using DrillSergeant.Build;
-using Nuke.Common;
-
-// ReSharper disable once CheckNamespace
-// ReSharper disable once RedundantExtendsListEntry
-class Build : NukeBuild, ITest, IPublish
-{
-    public static int Main() => Execute<Build>();
-}
