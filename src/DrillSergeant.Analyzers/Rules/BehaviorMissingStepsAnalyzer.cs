cadbf051c25aa2eaf7592a79c102e5265f225a53
Michael Vastarelli <mike@bitcobblers.com>
2023-11-05 15:33:15 -0500

    Feature/add code analyzers (#92)
    +semver:minor
    
    * Added additional path filters and misc. cleanups. (#84)
    
    * Added additional path filters and misc. cleanups.
    
    * Pull requests will always force CICD to run.
    
    * Code coverage runs as part of CI now.
    
    * Created signing key.
    
    * Updated package versions. (#85)
    
    * Feature/add signing key (#86)
    
    * Created signing key.
    
    * All assemblies (except _build) are now signed.
    
    * Fixed syntax error in conditional.
    
    * Disabled signing for build projects.
    
    * Add quality scanning to CodeQL (#87)
    
    * Added quality scanning to CodeQL.
    
    * Update .github/workflows/codeql.yml
    
    * Updated package versions. (#85)
    
    * Feature/add signing key (#86)
    
    * Created signing key.
    
    * All assemblies (except _build) are now signed.
    
    * Fixed syntax error in conditional.
    
    * Disabled signing for build projects.
    
    * Verb is now set when adding normal steps. (#88)
    
    * Fixed syntax issues. (#89)
    
    * Fixed syntax issues.
    
    * CodeQL will run for all main/dev commits.
    
    * Updated trigger filters.
    
    * Removed invalid args.
    
    * Add quality scanning to CodeQL (#87)
    
    * Added quality scanning to CodeQL.
    
    * Update .github/workflows/codeql.yml
    
    * Verb is now set when adding normal steps. (#88)
    
    * Fixed syntax issues. (#89)
    
    * Fixed syntax issues.
    
    * CodeQL will run for all main/dev commits.
    
    * Updated trigger filters.
    
    * Removed invalid args.
    
    * Added Analyzers project to the solution.
    
    * Migrated code from previous branch br_add_analyzers.
    
    * Sorted packages.
    
    * Added scripts and nuspec file.
    
    * Fixed folder placement.
    
    * Switched to GitVersion.MSBuild.  Added nuspec for analyzers.
    
    * Disabled RS2007 lint rule
    
    * Suppress RS2007 for Release builds.
    
    * Deleted global.json
    
    * Set fetch-depth to 0 for GitVersion.MSBuild.

---
+++ src/DrillSergeant.Analyzers/Rules/BehaviorMissingStepsAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace DrillSergeant.Analyzers.Rules;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class BehaviorMissingStepsAnalyzer : DiagnosticAnalyzer
+{
+    public const string DiagnosticId = "DS0002";
+
+    private const string Title = "Behavior method does not add any steps";
+    private const string MessageFormat = "Behavior method has no defined steps and will not do anything";
+    private const string Description = "At least one step should be defined within a behavior.";
+    private const string Category = "Design";
+
+    private static readonly DiagnosticDescriptor Rule = new(
+        DiagnosticId,
+        Title,
+        MessageFormat,
+        Category,
+        DiagnosticSeverity.Warning,
+        isEnabledByDefault: true,
+        description: Description);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
+        ImmutableArray.Create(Rule);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+        context.RegisterCodeBlockAction(AnalyzeBlock);
+    }
+
+    private void AnalyzeBlock(CodeBlockAnalysisContext context)
+    {
+        if (context.CodeBlock is not MethodDeclarationSyntax method)
+        {
+        }
+    }
+}
---
+++ test/DrillSergeant.Tests/Analyzers/BehaviorMethodAccessorAnalyzerTests.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Analyzer =
+    Microsoft.CodeAnalysis.CSharp.Testing.XUnit.AnalyzerVerifier<
+        DrillSergeant.Analyzers.Rules.BehaviorMethodAccessorAnalyzer>;
+
+using Fixer =
+    Microsoft.CodeAnalysis.CSharp.Testing.XUnit.CodeFixVerifier<
+        DrillSergeant.Analyzers.Rules.BehaviorMethodAccessorAnalyzer,
+        DrillSergeant.Analyzers.Fixes.BehaviorMethodAccessorFixProvider>;
+
+namespace DrillSergeant.Tests.Analyzers;
+
+public class BehaviorMethodAccessorAnalyzerTests
+{
+    private const string SampleCode = @"
+using System;
+
+namespace MyNamespace;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class BehaviorAttribute : Attribute
+{
+}
+
+public class Ignored
+{
+    [Behavior]
+    $ACCESSOR$ void InvalidBehaviorMethod()
+    {
+    }
+}";
+
+    private static string GetSample(string accessor) => SampleCode.Replace("$ACCESSOR$", accessor);
+
+    public static IEnumerable<object[]> NonPublicAccessors
+    {
+        get
+        {
+            return new[]
+            {
+                new[] { "private" },
+                new[] { "protected" },
+                new[] { "internal" }
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(NonPublicAccessors))]
+    public async Task IdentifiesNonPublicBehaviorMethods(string accessor)
+    {
+        // Arrange.
+        var code = GetSample(accessor);
+
+        // Act.
+        var result = Analyzer.Diagnostic()
+            .WithLocation(13, 5)
+            .WithArguments("InvalidBehaviorMethod");
+
+        // Assert.
+        await Analyzer.VerifyAnalyzerAsync(code, result);
+    }
+
+    [Theory]
+    [MemberData(nameof(NonPublicAccessors))]
+    public async Task ApplyingFixReplacesNonPublicAccessorWithPublic(string accessor)
+    {
+        // Arrange.
+        var invalidCode = GetSample(accessor);
+        var expectedCode = GetSample("public");
+
+        // Act.
+        var result = Fixer.Diagnostic()
+            .WithLocation(13, 5)
+            .WithArguments("InvalidBehaviorMethod");
+
+        // Assert.
+        await Fixer.VerifyCodeFixAsync(invalidCode, result, expectedCode);
+    }
+
+    [Theory]
+    [InlineData("protected virtual", "public virtual", 5)]
+    [InlineData("virtual protected", "virtual public", 5)]
+    public async Task FixingNonPublicAccessorPreservesKeywordOrder(string nonPublicModifiers, string publicModifiers, int column)
+    {
+        // Arrange.
+        var invalidCode = GetSample(nonPublicModifiers);
+        var expectedCode = GetSample(publicModifiers);
+
+        // Act.
+        var result = Fixer.Diagnostic()
+            .WithLocation(13, column)
+            .WithArguments("InvalidBehaviorMethod");
+
+        // Assert.
+        await Fixer.VerifyCodeFixAsync(invalidCode, result, expectedCode);
+    }
+}
